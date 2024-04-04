e0ed33330f00d001ba1a055d107a742af1d05d55
Yisak Tamiru <113479036+yisak1200@users.noreply.github.com>
2024-04-04 11:34:50 +0300

    update project

--- CNET_DATABASE_ACCESS_MANAGEMENT/settings.py
+++ CNET_DATABASE_ACCESS_MANAGEMENT/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-e*i*45dftor95@n7ncdcsqqs*t^h1dbg2iqfuuv9sdue#j)g-x'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG',cast=bool)
 
 ALLOWED_HOSTS = ['*']
 
